a13bf47f0c59ad9a67ea31d73cf80e154b2ea856
Arcom <arcom@arcominf.com.br>
2017-03-13 11:41:34 -0300

    versao final

--- pxeimg/startup.sh
+++ pxeimg/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i "s/pxeipaddress/$SERVIDORPXE" /var/lib/tftpboot/pxelinux.cfg/default 
+sed -i "s/pxeipaddress/$SERVIDORPXE/" /var/lib/tftpboot/pxelinux.cfg/default 
 /usr/sbin/in.tftpd -L --user root -a 0.0.0.0:69 -s -B1468 -v /var/lib/tftpboot
